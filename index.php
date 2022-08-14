47178885d358785e0063c0bd7d3d78cf3db1bd2c
Marike <marike.puranen@gmail.com>
2022-08-14 16:18:14 +0300

    add and style user post column

--- index.php
+++ index.php
@@ -16,6 +16,13 @@ include("includes/header.php");
                         ?>
                 </div>
             </div>
+            <div class="main_column column">
+                <form class="post_form" action="index.php" method="POST">
+                    <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+                    <input type="submit" name="post" id="post_button" value="Post">
+                    <hr>
+                </form>
+            </div>
         </div>
     </body>
 </html>
