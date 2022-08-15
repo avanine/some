5d831bdfd0574222f5094baa77b40c3af43e3c01
Marike <marike.puranen@gmail.com>
2022-08-15 15:05:11 +0300

    like button works

--- like.php
+++ like.php
@@ -38,13 +38,22 @@
     //like button
     if (isset($_POST['like_button'])) {
         $total_likes++;
-        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE post_id='$post_id'");
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
         $total_user_likes++;
         $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
         $insert_user = mysqli_query($con, "INSERT INTO likes VALUES(NULL, '$userLoggedIn', '$post_id')");
+
+        //insert notification
     }
 
     //unlike button
+    if (isset($_POST['unlike_button'])) {
+        $total_likes--;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
+        $total_user_likes--;
+        $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, "DELETE FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
+    }
 
     //check for previous likes
     $check_query = mysqli_query($con, "SELECT * FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
