69a66d3ec91a60e59627f00f9aa4bb3cda2770db
Marike <marike.puranen@gmail.com>
2022-08-15 10:27:22 +0300

    posting comments works

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -99,6 +99,23 @@ class Post {
                     $last_name = $user_row['last_name'];
                     $profile_pic = $user_row['profile_pic'];
 
+                    ?>
+
+                    <script>
+                        //use php to check which comments to show
+                        function toggle<?php echo $id; ?>() {
+                            var element = document.getElementById("toggleComment<?php echo $id; ?>");
+
+                            if (element.style.display == "block") {
+                                element.style.display = "none";
+                            } else {
+                                element.style.display = "block";
+                            }
+                        }
+                    </script>
+
+                    <?php
+
                     //timeframe
                     $date_time_now = date("Y-m-d H:i:s");
                     $start_date = new DateTime($date_time); //time of post
@@ -151,7 +168,7 @@ class Post {
                         }
                     }
 
-                    $str .= "<div class='status_post'>
+                    $str .= "<div class='status_post' onClick='javascript:toggle$id()'>
 
                                 <div class='post_profile_pic'>
                                     <img src='$profile_pic' width='50'>
@@ -164,6 +181,9 @@ class Post {
                                     $body
                                     <br>
                                 </div>
+                            </div>
+                            <div class='post_comment' id='toggleComment$id' style='display: none;'>
+                                <iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'></iframe>
                             </div>
                             <hr>";
                 }
