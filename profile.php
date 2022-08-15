3cc5d569c9cfcd30d2200ebcde4843973ed8bc29
Marike <marike.puranen@gmail.com>
2022-08-15 21:31:46 +0300

    add friend button

--- profile.php
+++ profile.php
@@ -1,5 +1,7 @@
 <?php
 include("includes/header.php");
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
 
 if (isset($_GET['profile_username'])) {
     $username = $_GET['profile_username'];
@@ -25,6 +27,30 @@ if (isset($_GET['profile_username'])) {
                 <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
                 <p><?php echo "Friends: " . $num_friends; ?></p>
             </div>
+
+            <form action="<?php echo $username; ?>">
+                <?php
+                $profile_user_obj = new User($con, $username);
+
+                if ($profile_user_obj->isClosed()) {
+                    header("Location: user_closed.php");
+                }
+
+                $logged_in_user_obj = new User($con, $userLoggedIn);
+
+                if ($userLoggedIn != $username) {
+                    if ($logged_in_user_obj->isFriend($username)) {
+                        echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+                    } else if ($logged_in_user_obj->didReceiveRequest($username)) {
+                        echo '<input type="submit" name="respond_request" class="warning" value="Respond to Request"><br>';
+                    } else if ($logged_in_user_obj->didSendRequest($username)) {
+                        echo '<input type="submit" name="" class="default" value="Request Sent"><br>';
+                    } else {
+                        echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
+                    }
+                }
+                ?>
+            </form>
         </div>
 
             <div class="main_column column">
