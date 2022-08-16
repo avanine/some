8ef6eb8964c8d3f86bbf6e2ea40b6b9163ff6df7
Marike <marike.puranen@gmail.com>
2022-08-16 20:40:52 +0300

    add friend and remove friend functionality

--- profile.php
+++ profile.php
@@ -10,6 +10,21 @@ if (isset($_GET['profile_username'])) {
 
     $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
 }
+
+if (isset($_POST['remove_friend'])) {
+    $user = new User($con, $userLoggedIn);
+    $user->removeFriend($username);
+}
+
+if (isset($_POST['add_friend'])) {
+    $user = new User($con, $userLoggedIn);
+    $user->sendRequest($username);
+}
+
+if (isset($_POST['respond_request'])) {
+    header("Location: requests.php");
+}
+
 ?>
 
         <style type="text/css">
@@ -28,7 +43,7 @@ if (isset($_GET['profile_username'])) {
                 <p><?php echo "Friends: " . $num_friends; ?></p>
             </div>
 
-            <form action="<?php echo $username; ?>">
+            <form action="<?php echo $username; ?>" method="POST">
                 <?php
                 $profile_user_obj = new User($con, $username);
 
